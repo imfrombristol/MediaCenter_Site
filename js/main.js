c056ef6358031af4ee5e3374b24b2609a3d63a71
Nick Del Nara <30962230+imfrombristol@users.noreply.github.com>
2018-01-23 17:55:21 +0200

    second

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+        items: 1,
+        // autoHeight: true,
+        loop: true,
+        margin: 10,
+        nav: true,
+  });
+});
+
