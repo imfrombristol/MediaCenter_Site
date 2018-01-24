4664a94a622a7a21c3dcbc018e3ea014bf11d318
Nick Del Nara <30962230+imfrombristol@users.noreply.github.com>
2018-01-24 23:37:22 +0200

    jquerymenu

--- js/main.js
+++ js/main.js
@@ -2,9 +2,18 @@ $(document).ready(function() {
   $(".owl-carousel").owlCarousel({
         items: 1,
         // autoHeight: true,
+        animateIn: 'fadeInRightBig',
+        animateOut: 'fadeOut',
         loop: true,
         margin: 10,
         nav: true,
   });
 });
 
+let leftHeight = $(".outlet_left").innerHeight();
+let rightHeight = $(".outlet_right").innerHeight();
+
+
+$(".outlet_right").height(function(i, value) {
+    return value+leftHeight
+})
