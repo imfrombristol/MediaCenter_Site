166baa31ff16dd41fd9c81268d92ac1db4f27ffb
Nick Del Nara <30962230+imfrombristol@users.noreply.github.com>
2018-01-26 16:20:18 +0200

    fulllayout

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $(".owl-carousel").owlCarousel({
         items: 1,
         // autoHeight: true,
-        animateIn: 'fadeInRightBig',
+        // animateIn: 'fadeInRightBig',
         animateOut: 'fadeOut',
         loop: true,
         margin: 10,
@@ -10,10 +10,22 @@ $(document).ready(function() {
   });
 });
 
-let leftHeight = $(".outlet_left").innerHeight();
-let rightHeight = $(".outlet_right").innerHeight();
-
-
-$(".outlet_right").height(function(i, value) {
-    return value+leftHeight
+$(document).ready(function(){
+    $(".outlet_wrap").slice(1).hide();
+    $('.videoproduction li').eq(0).addClass("active");
+    $('.videoproduction li a').click(function(e){
+        e.preventDefault();
+        let aim = $(this).attr("href");
+        $(this)
+          .parent()
+          .addClass("active");
+        $(this)
+          .parent()
+          .siblings()
+          .removeClass("active");
+          $(aim).show();
+          $(aim)
+            .siblings(".outlet_wrap")
+            .hide();
+    })
 })
